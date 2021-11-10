9d9a41b9000a0fac08538d0b1480dcf1961d7c5d
shikhin <shikhin@metronlabs.com>
2021-11-10 17:23:15 +0530

    minor changes

--- DataConnectors/Digital shadows/DigitalShadowsConnectorAzureFunction/DS_poller.py
+++ DataConnectors/Digital shadows/DigitalShadowsConnectorAzureFunction/DS_poller.py
@@ -21,9 +21,6 @@ class poller:
         logging.info("From time: %s", self.after_time)
         logging.info("to time: %s", self.before_time)
 
-    def post_comments(self, comment):
-        self.AS_obj.post_data(json.dumps(comment), constant.COMMENTS_LOG_NAME)
-
 
     def post_azure(self, response, item):
         """
@@ -36,31 +33,32 @@ class poller:
         json_obj[0]['triage_updated_time'] = item['updated']
         self.AS_obj.post_data(json.dumps((json_obj[0])), constant.LOG_NAME)
 
+    def get_data(self):
+        """
+            getting the incident and alert data from digital shadows
+        """
+        try:
+            event_dataJSON = self.DS_obj.get_triage_events(str(self.before_time), str(self.after_time))
+            event_data = json.loads(event_dataJSON)
+            triage_id = []
+            for event in event_data:
+                if(event is not None):
+                    triage_id.append(event_data[0]['triage-item-id'])
+
+            item_data = json.loads(self.DS_obj.get_triage_items(triage_id))
+        except (ValueError, IndexError, UnboundLocalError):
+            logging.info(event_dataJSON)
+            logging.info("JSON is of invalid format or no new incidents or alerts are found")
+        
+        return item_data
+
     def poll(self):
         """
             main polling function, 
             makes api calls in following fashion:
             triage-events --> triage-items --> incidents and alerts 
         """
-        
-        try:
-            event_dataJSON = self.DS_obj.get_triage_events(str(self.before_time), str(self.after_time))
-            event_data = json.loads(event_dataJSON)
-            
-            triages_str = str(event_data[0]['triage-item-id'])
-            for event in event_data:
-                #logging.info(event)
-                if(event is not None):
-                    triages_str = triages_str + "&id=" + str(event['triage-item-id'])
-
-        except (ValueError, IndexError):
-            logging.info("JSON is of invalid format or no new incidents or alerts are found")
-
-        try:
-            item_data = json.loads(self.DS_obj.get_triage_items(triages_str))
-            comments = json.loads(self.DS_obj.get_triage_comments(triages_str))
-        except UnboundLocalError:
-            logging.info("No new incidents or alerts found")
+        item_data = self.get_data()
                     
         try:
             #sending data to sentinel
@@ -72,10 +70,8 @@ class poller:
                 elif(item['source']['alert-id'] is not None):
                     response = self.DS_obj.get_alerts(item['source']['alert-id'])
                     self.post_azure(response, item)
-            
-            for c in comments:
-                self.post_comments(c)
 
 
         except (KeyError, TypeError, UnboundLocalError):
+            logging.info(item_data)
             logging.info("Key error or type error has occured or no new incidents or alerts are found")
--- DataConnectors/Digital shadows/DigitalShadowsConnectorAzureFunction/constant.py
+++ DataConnectors/Digital shadows/DigitalShadowsConnectorAzureFunction/constant.py
@@ -3,4 +3,3 @@ DAYS = 5
 MINUTE = 15                                 #buffer time for current time
 SHARE_NAME = 'funcstatemarkershare'
 FILE_PATH = 'funcstatemarkerfile'
-COMMENTS_LOG_NAME = "DigitalShadowsComments"
